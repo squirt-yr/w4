e42cb6f22778f66b6c1b3448a0952b0f808b83b2
squirt-yr <113569241+squirt-yr@users.noreply.github.com>
2022-09-29 11:48:57 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -1,17 +1,21 @@
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+//ここまでが変数宣言
 
+//乱数の発生
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
+//入れ替えたい文字列を格納
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
+//格納されている文字列からランダムに選ぶ
 randomize.addEventListener('click', result);
 
 function result() {
